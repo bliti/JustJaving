f8bef4cc9405d0b8ca84ece8ce7fd86832d535b7
pr <pr@192.168.56.1>
2016-01-10 18:29:03 -0400

    some random drake

---
+++ JustJaving/src/justjaving/JustJaving.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package justjaving;
+
+/**
+ *
+ * @author pr
+ */
+public class JustJaving {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*
+        If you are reading this:
+        Im just toying with java, netbeans, and github.
+        */
+        
+        System.out.println("I know when that hotline bling,");
+        System.out.println("it could only mean one thing.");
+        
+        System.out.println("You used to call me on my cellphone");
+        System.out.println("late night when you need my love.");
+    }
+    
+}
